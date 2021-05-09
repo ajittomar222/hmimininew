df0477f9d7a2c582cc3f87faa7854baa8734a5aa
Ajit Tomar <ajittomar222@gmail.com>
2021-05-09 20:14:11 +0530

    settings

--- django_pro/settings.py
+++ django_pro/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os,django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,5 +131,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'adwaitpatil1199@gmail.com'
-EMAIL_HOST_PASSWORD = 'kqzyasklgykciyxx'
+EMAIL_HOST_USER = '#'
+EMAIL_HOST_PASSWORD = '#'
+django_heroku.settings(locals())
